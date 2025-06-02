c81a422e7dffbb79156364889daafdb5bdf591eb
Fcontrvillaa <fcontreras+1@gmail.com>
2025-06-02 19:49:45 +0000

    menu princ

---
+++ pizza_jat.py
@@ -0,0 +1,17 @@
+
+def menu_principal():
+    """Muestra el menú principal de opciones."""
+    print("\n--- Menú Principal Pizza JAT ---")
+    print("1. Personalizar Pizza")
+    print("2. Mostrar Pizza Actual")
+    print("3. Estimar Tiempo y Confirmar Orden")
+    print("4. Salir")
+
+def personalizar_pizza():
+    print("\n--- Personalizar Pizza ---")
+    print("a. Cambiar Tipo de Masa")
+    print("b. Cambiar Tipo de Salsa")
+    print("c. Modificar Ingredientes")
+    print("d. Volver al Menú Principal")
+
+def cambiar_masa(pizza):
